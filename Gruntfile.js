8deaaf3e2498b419795c74dd2d844c176810c8f3
Giancarlo Anemone <giancarlo@uber.com>
2014-08-25 18:06:30 -0700

    Update build pipeline

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,43 @@
-var loadGruntConfig = require('load-grunt-config');
-
 module.exports = function (grunt) {
-  // Load up all the grunt config files in /grunt
-  loadGruntConfig(grunt, {});
-  // Load all the grunt tasks
   require('load-grunt-tasks')(grunt);
-
-  // Aliases
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('server', ['nodemon:dev']);
+  // Load up all the grunt config files in /grunt
+  grunt.initConfig({
+    browserify: {
+      app: {
+        files: [{
+          expand: true,
+          cwd: 'public/javascripts/',
+          src: ['main.js'],
+          dest: 'build/javascripts/'
+        }],
+      }
+    },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'public/stylesheets',
+          dest: 'build/stylesheets',
+          src: [
+            'main.scss'
+          ],
+          ext: '.css'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: [
+          'public/stylesheets/**/*.scss',
+          'public/javascripts/**/*.js'
+        ],
+        tasks: ['sass', 'browserify'],
+        options: {
+          livereload: true,
+          spawn: false,
+          interrupt: true,
+        }
+      }
+    }
+  });
 };
