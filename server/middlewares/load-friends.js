8546dff7245de0602997fd5771bb0cf2b5e5384b
Giancarlo Anemone <ganemone@gmail.com>
2014-09-12 14:53:16 -0400

    Add middleware to load friends

--- server/middlewares/load-friends.js
+++ server/middlewares/load-friends.js
@@ -1,5 +1,14 @@
-var User = require('../models/user.js');
+var async = require('async');
 
 module.exports = function(req, res, next) {
-
+  var user = res.locals.user;
+  async.series([
+    user.loadFriends.bind(user),
+    user.loadSecondDegreeFriends.bind(user)
+  ], function(err, result) {
+    if (err) {
+      return next(new HttpError('Internal Server Error - Failed to load user friends', 500));
+    }
+    return next();
+  });
 }
