208b76a5e83cff2932ed658f761a6f5137fe9cc2
Giancarlo Anemone <ganemone@gmail.com>
2014-09-12 13:43:27 -0400

    Add middleware for loading friends and test util for setting up friends

---
+++ server/middlewares/load-friends.js
@@ -0,0 +1,5 @@
+var User = require('../models/user.js');
+
+module.exports = function(req, res, next) {
+
+}
---
+++ test/util/setup-rosterusers.js
@@ -0,0 +1,37 @@
+var db = require('../../server/util/db');
+var config = require('../../config/index.js');
+
+module.exports = function(username, friends) {
+
+  before(function (done) {
+    var query = "INSERT INTO rosterusers (username, jid) VALUES ";
+    var data = [];
+    var jid = username + "@" + config.ip;
+    for(var i = 0; i < friends.length - 1; i++) {
+      var friend = friends[i];
+      var friendJID = friend + "@" + config.ip;
+      query += "(?, ?), (?, ?), ";
+      data.push(username);
+      data.push(friendJID);
+      data.push(jid);
+      data.push(friend);
+    }
+    var lastFriend = friends[friends.length - 1];
+    var lastFriendJID = lastFriend + '@' + config.ip;
+    query += "(?, ?), (?, ?)";
+    data.push(username);
+    data.push(lastFriendJID);
+    data.push(jid);
+    data.push(lastFriend);
+    db.executeQueryWithData(query, data, function(err, rows, fields) {
+      done();
+    });
+  });
+
+  after(function (done) {
+    var query = 'DELETE FROM rosterusers';
+    db.directQuery(query, function() {
+      done();
+    });
+  });
+};
