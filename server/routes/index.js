bed02915efe287fd6339a5e72f1294507af7f679
Giancarlo Anemone <ganemone@gmail.com>
2014-09-12 10:34:46 -0400

    Add confirmation view and route

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,7 +2,10 @@ var controllers = require('../controllers');
 
 module.exports = function setRoutes(app) {
   app.get('/health', controllers.health.index);
+
+  // Forgot Password
   app.get('/password/forgot', controllers.forgotPassword.index);
   app.get('/password/reset', controllers.forgotPassword.reset);
   app.get('/password/forgot/trigger/:email', controllers.forgotPassword.trigger);
+  app.get('/password/forgot/confirmation', controllers.forgotPassword.confirmation);
 };
