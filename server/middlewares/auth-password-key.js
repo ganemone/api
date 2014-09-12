15cff26ff81be70e3e8403b92cae9c878efcfbef
Giancarlo Anemone <ganemone@gmail.com>
2014-09-12 13:39:45 -0400

    Add password key auth middleware

---
+++ server/middlewares/auth-password-key.js
@@ -0,0 +1,28 @@
+var User = require('../models/user.js');
+var HttpError = require('../util/http-error.js');
+
+module.exports = function(req, res, next) {
+  if (!req.query.username) {
+    return new HttpError('Missing username parameter', 406);
+  }
+  if (!req.query.key) {
+    return new HttpError('Missing password key parameter', 406);
+  }
+
+  var user = new User({
+  	username: req.query.username,
+  	passwordKey: req.query.key
+  });
+
+  user.hasValidPasswordKey(function(err, result) {
+  	if (err) {
+  		console.error(err);
+  		return next(new HttpError('Internal Server Error - Failed Database transaction', 500));
+  	}
+  	if (!result) {
+  		return next(new HttpError('Invalid username/password key combination', 406));
+  	}
+  	res.locals.user = user;
+  	return next();
+  });
+}
