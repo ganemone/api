38f907763a56db6963e1c44ea1755d8caaa1911c
Giancarlo Anemone <ganemone@gmail.com>
2014-09-15 12:27:35 -0400

    Add tests for the message middleware

---
+++ test/server/middlewares/message.js
@@ -0,0 +1,38 @@
+var assert = require('assert');
+var message = require('../../../server/middlewares/message.js');
+
+describe('A messsage middleware', function () {
+  
+  it('should set res.locals.message if passed as a query parameter', function (done) {
+    var mockReq = {
+      query: {
+        message: 'message'
+      }
+    };
+    var mockRes = {
+      locals: {}
+    };
+    var mockNext = function(err) {
+      assert.ifError(err, 'Should execute without error');
+      assert.equal(mockRes.locals.message, 'message', 'Should set res.locals.message correctly');
+      done();
+    }
+    message(mockReq, mockRes, mockNext);
+  });
+
+  it('should not set res.locals.message if not passed as a query parameter', function (done) {
+    var mockReq = {
+      query: {}
+    };
+    var mockRes = {
+      locals: {}
+    };
+    var mockNext = function(err) {
+      assert.ifError(err, 'Should execute without error');
+      assert.ifError(mockRes.locals.message, 'Should not set mockRes.locals.message');
+      done();
+    }
+    message(mockReq, mockRes, mockNext);
+  });
+
+});
