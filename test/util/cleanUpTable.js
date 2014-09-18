b6b76b8358a57a05074642a29855a7376c140388
Giancarlo Anemone <ganemone@gmail.com>
2014-09-17 22:16:53 -0400

    Add test util for deleting data from tables

---
+++ test/util/cleanUpTable.js
@@ -0,0 +1,12 @@
+var db = require('../../server/util/db.js');
+
+module.exports = function(table) {
+  after(function (done) {
+    db.queryWithData('DELETE FROM ??', [table], function(err, rows) {
+      if (err) {
+        console.error('Failed to clean up table', err);
+      };
+      done();
+    });
+  });
+}
