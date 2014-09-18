4e3ebf1b471cf344a974f36a6104773767dbde1d
Giancarlo Anemone <ganemone@gmail.com>
2014-09-17 22:19:54 -0400

    Fix test util that sets up username_phone_email data

--- test/util/setUpUsernamePhoneEmail.js
+++ test/util/setUpUsernamePhoneEmail.js
@@ -3,7 +3,7 @@ var db = require('../../server/util/db.js');
 module.exports = function(username, ccode, phone, email) {
   before(function (done) {
     var query = 'INSERT INTO username_phone_email (username, ccode, phone, email) VALUES (?)';
-    var data = [username, ccode, phone, email];
+    var data = [[username, ccode, phone, email]];
     db.queryWithData(query, data, function(err, rows) {
       done();
     });
