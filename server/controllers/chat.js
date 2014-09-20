8af1714154d2e8e6de8cd72074174ec029f7cfcf
Giancarlo Anemone <ganemone@gmail.com>
2014-09-20 12:59:43 -0400

    Implement create, join, and leave

--- server/controllers/chat.js
+++ server/controllers/chat.js
@@ -1,9 +1,61 @@
+// External Modules
+var async = require('async');
+var request = require('request');
+var url = require('url');
+// Internal Modules
+var Chat = require('../models/chat.js');
 
+// Endpoint
 exports.create = function (req, res, next) {
-
+  var user = res.locals.user;
+  var chat = res.locals.chat;
+  async.series([
+    chat.insert.bind(chat),
+    chat.insertParticipants.bind(chat),
+    function(result, cb) {
+      if (chat.type === 'group') {
+        notifyParticipants(chat, cb);
+      }
+    }
+  ], function (err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json(chat.toJSON()); 
+  });
 };
 
 exports.leave = function (req, res, next) {
-
+  var user = res.locals.user;
+  var chat = res.locals.chat;
+  chat.removeUser(user, function (err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json();
+  }); 
 };
+
+exports.join = function (req, res, next) {
+  var user = res.locals.user;
+  var chat = res.locals.chat;
+  chat.joinUser(user, function (err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json();
+  });
+};
+
+function notifyParticipants(chat, cb) {
+  var notifyURL = url.format({
+    protocol: 'http:',
+    host: 'localhost',
+    pathname: '/notify/new_group',
+    query: {
+      id: chat.id
+    }
+  });
+  request.get(notifyURL, cb);
+}
 
