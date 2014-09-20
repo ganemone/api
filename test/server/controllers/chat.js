1e187ddd71e97de6965e5035843471d009455b10
Giancarlo Anemone <ganemone@gmail.com>
2014-09-20 15:27:06 -0400

    Add tests for chat controller join and leave

--- test/server/controllers/chat.js
+++ test/server/controllers/chat.js
@@ -11,9 +11,15 @@ describe('A Chat Controller', function () {
       it('should work', function (done) {
         testCreate('121', done);
       });
+      it('should handle errors', function (done) {
+        testCreate('121', done);
+      });
     });
     describe('a thought', function () {
       it('should work', function (done) {
+        testCreate('thought', done);
+      });
+      it('should handle errors', function (done) {
         testCreate('thought', done);
       });
     });
@@ -21,12 +27,105 @@ describe('A Chat Controller', function () {
       it('should work', function (done) {
         testCreate('group', done);
       });
+      it('should handle errors', function (done) {
+        testCreate('group', done);
+      });
     });
-    describe('handle errors', function () {
-      
+  });
+  describe('leave', function () {
+    it('should work', function (done) {
+      var mockRemoveUser = getMockWithCB(['object', 'function'], null, true);
+      var mockNext = new Mock(function noop() {});
+      var mockJSON = getMockWithDone([], [mockRemoveUser], [mockNext], done);
+      var mockRes = {
+        locals: {
+          user: {},
+          chat: {
+            removeUser: mockRemoveUser.getFn()
+          }
+        },
+        json: mockJSON.getFn()
+      };
+      chat.leave({}, mockRes, mockNext.getFn());
+    });
+    it('should handle errors', function (done) {
+      var mockRemoveUser = getMockWithCB(['object', 'function'], new Error('Some error'));
+      var mockJSON = new Mock(function noop() {});
+      var mockNext = getMockWithDone(['object'], [mockRemoveUser], [mockJSON], done);
+
+      var mockRes = {
+        locals: {
+          user: {},
+          chat: {
+            removeUser: mockRemoveUser.getFn()
+          }
+        },
+        json: mockJSON.getFn()
+      };
+      chat.leave({}, mockRes, mockNext.getFn());
+    });
+  });
+  describe('join', function () {
+    it('should work', function (done) {
+      var mockJoinUser = getMockWithCB(['object', 'function'], null, true);
+      var mockNext = new Mock(function noop() {});
+      var mockJSON = getMockWithDone([], [mockJoinUser], [mockNext], done);
+      var mockRes = {
+        locals: {
+          user: {},
+          chat: {
+            joinUser: mockJoinUser.getFn()
+          }
+        },
+        json: mockJSON.getFn()
+      };
+      chat.join({}, mockRes, mockNext.getFn());
+    });
+    it('should handle errors', function (done) {
+      var mockJoinUser = getMockWithCB(['object', 'function'], new Error('Some error'));
+      var mockJSON = new Mock(function noop() {});
+      var mockNext = getMockWithDone(['object'], [mockJoinUser], [mockJSON], done);
+
+      var mockRes = {
+        locals: {
+          user: {},
+          chat: {
+            joinUser: mockJoinUser.getFn()
+          }
+        },
+        json: mockJSON.getFn()
+      };
+      chat.join({}, mockRes, mockNext.getFn());
     });
   });
 });
+
+function getMockWithCB(args, error, result) {
+  return new Mock(function () {
+    assert.equal(arguments.length, args.length);
+    for(var i = 0; i < arguments.length; i++) {
+      assert.equal(typeof arguments[i], args[i]);
+    }
+    var cb = arguments[arguments.length - 1];
+    cb(error, result);
+  });
+}
+
+function getMockWithDone(args, called, notCalled, done) {
+  return new Mock(function () {
+    assert.equal(arguments.length, args.length);
+    for(var i = 0; i < arguments.length; i++) {
+      assert.equal(typeof arguments[i], args[i]);
+    }
+    for (var i = 0; i < called.length; i++) {
+      called[i].assertCalledOnce();
+    }
+    for (var i = 0; i < notCalled.length; i++) {
+      notCalled[i].assertNotCalled();
+    };
+    done();
+  });
+}
 
 function getMockToJSON() {
   return new Mock(function mockToJSON() {
@@ -34,25 +133,25 @@ function getMockToJSON() {
   });
 }
 
-function getMockInsert() {
+function getMockInsert(err) {
   return  new Mock(function mockInsert(cb) {
     assert.equal(typeof cb, 'function');
-    cb(null, {}); 
+    cb(err, {}); 
   }); 
 }
 
-function getMockInsertParticipants() {
+function getMockInsertParticipants(err) {
   return new Mock(function mockInsertParticipants(cb) {
     assert.equal(typeof cb, 'function');
-    cb(null, {});
+    cb(err, {});
   });
 }
 
-function getMockNotifyParticipants() {
+function getMockNotifyParticipants(err) {
   return new Mock(function mockNotifyParticipants(chat, cb) {
     assert.ok(chat.id);
     assert.equal(typeof cb, 'function');
-    cb();
+    cb(err);
   });
 }
 
@@ -71,17 +170,22 @@ function getMockRes(type, mockJSON, mockInsert, mockInsertParticipants, mockToJS
   };
 }
 
-function getMockNext() {
+function getMockNext(error, done) {
   return new Mock(function mockNext(err) {
-    assert.ifError(err);
+    if (error) {
+      assert.ok(err);
+      done();
+    } else {
+      assert.ifError(err);
+    }
   });
 }
 
-function testCreate(type, done) {
-  var mockInsert = getMockInsert();
-  var mockInsertParticipants = getMockInsertParticipants();
-  var mockNotifyParticipants = getMockNotifyParticipants();
-  var mockNext = getMockNext();
+function testCreate(type, done, error) {
+  var mockInsert = getMockInsert(error);
+  var mockInsertParticipants = getMockInsertParticipants(error);
+  var mockNotifyParticipants = getMockNotifyParticipants(error);
+  var mockNext = getMockNext(error, done);
   var mockToJSON = getMockToJSON();
 
   chat.__set__({
