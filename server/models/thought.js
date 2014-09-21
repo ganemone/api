c0befc9e0bd9394f343ba912ab48b85954a9c592
Giancarlo Anemone <ganemone@gmail.com>
2014-09-21 16:17:40 -0400

    Add function to get degree from user

--- server/models/thought.js
+++ server/models/thought.js
@@ -1,12 +1,13 @@
 var db = require('../util/db.js');
 var HttpError = require('../util/http-error.js');
+var usernameToJID = require('../util/usernameToJID.js');
 
 function Thought(cid) {
   this.id = cid;
 }
 
 Thought.prototype.load = function(cb) {
-  var query = 'SELECT * FROM confessions WHERE id = ?';
+  var query = 'SELECT * FROM confessions WHERE confession_id = ?';
   var data = [this.id];
   var self = this;
   db.queryWithData(query, data, function(err, rows) {
@@ -21,6 +22,24 @@ Thought.prototype.load = function(cb) {
       return cb(null, true);
     }
     cb(null, false);
+  });
+};
+
+Thought.prototype.getDegreeFromUser = function(username, cb) {
+  var query = 'SELECT count(*) AS count FROM rosterusers WHERE jid = ? AND username = ?;';
+  var jid = usernameToJID(this.username);
+  var data = [jid, username];
+  if (username === this.username) {
+    cb(null, 0);
+  }
+  db.queryWithData(query, data, function(err, result) {
+    if(err) {
+      return cb(new HttpError('Failed to get degree from user', 500, err));
+    }
+    var degree = (result[0].count > 0)
+      ? 1
+      : 2;
+    cb(null, degree);
   });
 };
 
