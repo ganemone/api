e90e94e0c28b8d2e77105afcd8676fc596314936
Giancarlo Anemone <ganemone@gmail.com>
2014-09-24 21:58:23 -0400

    Add loading/parsing of vcards to friends controller

--- server/controllers/friends.js
+++ server/controllers/friends.js
@@ -1,4 +1,8 @@
-
+var async = require('async');
+var _ = require('underscore');
+var db = require('../util/db.js');
+var HttpError = require('../util/http-error.js');
+var parseString = require('xml2js').parseString;
 // Endpoint: /friends
 // Method: GET
 // Authorization: Basic
@@ -11,10 +15,52 @@
 // }, ...]
 exports.index = function (req, res, next) {
   var user = res.locals.user;
-  res.json(user.friends);
+  getVCardFromUsernames(user.friends, function(err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json(result);
+  });
 };
 
 exports.pending = function (req, res, next) {
   var user = res.locals.user;
-  res.json(user.pendingFriends);
+  getVCardFromUsernames(user.pendingFriends, function(err, result) {
+    if (err) {
+      return next(err);
+    }
+    res.json(result);
+  });
 };
+
+function getVCardFromUsernames(usernames, cb) {
+  var query = 'SELECT * FROM vcard WHERE username IN (?)';
+  var data = [usernames];
+  db.queryWithData(query, data, function(err, rows) {
+    if(err) {
+      return cb(new HttpError('Failed to load vcards', 500, err));
+    }
+    cb(null, parseVCards(rows));
+  });
+}
+
+function parseVCards(rows, cb) {
+  var fns = _.map(rows, function(row) {
+    return parseVCard(row);
+  });
+  async.parallel(fns, cb);
+}
+
+function parseVCard(row) {
+  return function (cb) {
+    parseString(row.vcard, function(err, result) {
+      if (err) {
+        return cb(err);
+      }
+      return cb(null, {
+        username: row.username,
+        name: result.vCard.FN
+      });
+    });
+  };
+}
