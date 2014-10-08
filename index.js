ad8e5c8dcbc1bff6fa53c9705024898ddb41de71
William Guedes <willbrazil.usa@gmail.com>
2014-10-08 23:40:44 +0000

    Added uncaught exception handler

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 process.title = 'nodejs-api';
-
+process.on('uncaughtException', function (err) {
+  console.log('Uncaught Error: ', err);
+});
 var config = require('./config');
 var Server = require('./server/app.js');
 var server = new Server(config);
