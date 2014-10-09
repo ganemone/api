641b0bb157010a0aa6f5aa6782af7b571854e543
Giancarlo Anemone <ganemone@gmail.com>
2014-10-09 18:25:56 -0400

    Update http error to set message

--- server/util/http-error.js
+++ server/util/http-error.js
@@ -1,6 +1,7 @@
 function HttpError(msg, statusCode, error, redirect) {
   var err = new Error(msg);
   err.name = 'HttpError';
+  err.message = msg;
   err.statusCode = statusCode;
   err.redirect = redirect;
   err.error = error;
