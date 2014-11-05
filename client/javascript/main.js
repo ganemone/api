dd0df3d2735cace9152e9e03d8f8589be7d82b0d
Giancarlo Anemone <ganemone@gmail.com>
2014-11-05 17:58:48 -0500

    Simple script to send a message

--- client/javascript/main.js
+++ client/javascript/main.js
@@ -1,15 +1,17 @@
-var xmpp = require('node-xmpp-client');
-var client = new xmpp.Client({
-  jid: 'testuser',
+var Client = require('node-xmpp-client');
+var ltx = require('ltx');
+var client = new Client({
+  jid: 'testuser@versapp.co',
   password: 'password',
   host: 'versapp.co',
+  preferred: 'PLAIN',
   port: 4222
 });
 
 client.addListener('online', function(data) {
   console.log('Connected!: ', data);
-  var message = new xmpp.Element('message', {
-    to: 'd194ba04-1f84-421f-b935-5c19df244da5',
+  var message = new ltx.Element('message', {
+    to: 'd194ba04-1f84-421f-b935-5c19df244da5@versapp.co',
     'type': 'chat'
   }).c('body').t('some message body');
   client.send(message);
@@ -17,6 +19,7 @@ client.addListener('online', function(data) {
 });
 
 client.addListener('error', function(error) {
+  debugger;
   console.error(error);
   process.exit(1);
 });
