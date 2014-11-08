6b4a8ce2f59309882c7ad96715f99bfb0aaab7da
Giancarlo Anemone <ganemone@gmail.com>
2014-11-07 20:33:21 -0500

    Sending back isMine parameter for messages

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -1,3 +1,4 @@
+var _ = require('underscore');
 var db = require('../db/index');
 
 exports.all = function(req, res, next) {
@@ -6,8 +7,22 @@ exports.all = function(req, res, next) {
       chat_uuid: req.params.id
     }
   }).then(function(messages) {
-    res.json(messages);
+    var mapFunc = getConvertMessageFunction(res.locals.user.username);
+    var mappedMessages = _.map(messages, mapFunc);
+    return res.json(mappedMessages);
   }).catch(function(err){
     return next(err);
   });
 };
+
+function getConvertMessageFunction(username) {
+  return function convertMessageFunc(message) {
+    return {
+      body: message.body,
+      chat_uuid: message.chat_uuid,
+      id: message.id,
+      image_url: message.image_url,
+      isMine: (username === message.sender_username)
+    };
+  };
+}
